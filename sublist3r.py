7da90fb3bb229d770f41cff7f933e25c6294ddca
Ekechukwu David <63519479+davideke1@users.noreply.github.com>
2023-01-20 23:27:13 +0530

    Update sublist3r.py

--- sublist3r.py
+++ sublist3r.py
@@ -694,34 +694,34 @@ class DNSdumpster(enumratorBaseThreaded):
 #         return self.get_response(resp)
 
     # once the send_req is rewritten we don't need to call this function, the stock one should be ok
-    def enumerate(self):
-        while self.url != '':
-            resp = self.send_req(self.url)
-            resp = json.loads(resp)
-            if 'error' in resp:
-                self.print_(R + "[!] Error: Virustotal probably now is blocking our requests" + W)
-                break
-            if 'links' in resp and 'next' in resp['links']:
-                self.url = resp['links']['next']
-            else:
-                self.url = ''
-            self.extract_domains(resp)
-        return self.subdomains
+#     def enumerate(self):
+#         while self.url != '':
+#             resp = self.send_req(self.url)
+#             resp = json.loads(resp)
+#             if 'error' in resp:
+#                 self.print_(R + "[!] Error: Virustotal probably now is blocking our requests" + W)
+#                 break
+#             if 'links' in resp and 'next' in resp['links']:
+#                 self.url = resp['links']['next']
+#             else:
+#                 self.url = ''
+#             self.extract_domains(resp)
+#         return self.subdomains
 
-    def extract_domains(self, resp):
-        #resp is already parsed as json
-        try:
-            for i in resp['data']:
-                if i['type'] == 'domain':
-                    subdomain = i['id']
-                    if not subdomain.endswith(self.domain):
-                        continue
-                    if subdomain not in self.subdomains and subdomain != self.domain:
-                        if self.verbose:
-                            self.print_("%s%s: %s%s" % (R, self.engine_name, W, subdomain))
-                        self.subdomains.append(subdomain.strip())
-        except Exception:
-            pass
+#     def extract_domains(self, resp):
+#         #resp is already parsed as json
+#         try:
+#             for i in resp['data']:
+#                 if i['type'] == 'domain':
+#                     subdomain = i['id']
+#                     if not subdomain.endswith(self.domain):
+#                         continue
+#                     if subdomain not in self.subdomains and subdomain != self.domain:
+#                         if self.verbose:
+#                             self.print_("%s%s: %s%s" % (R, self.engine_name, W, subdomain))
+#                         self.subdomains.append(subdomain.strip())
+#         except Exception:
+#             pass
 
 
 class ThreatCrowd(enumratorBaseThreaded):
